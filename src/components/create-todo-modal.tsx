ecdd7a80346dec45264349d44251f72582972b86
KhangTNguyen <khangtnguyen@kms-technology.com>
2020-12-15 17:11:46 +0700

    add datepicker modal

--- src/components/create-todo-modal.tsx
+++ src/components/create-todo-modal.tsx
@@ -1,6 +1,6 @@
 import {CharacterStyles} from 'assets/character-styles';
 import {Colors} from 'assets/color';
-import React, {ReactElement, useState} from 'react';
+import React, {ReactElement, useCallback, useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -8,6 +8,8 @@ import {
   TouchableOpacity,
   View,
   Platform,
+  TouchableWithoutFeedback,
+  ScrollView,
 } from 'react-native';
 import Modal from 'react-native-modal';
 import {TodoPriority} from 'types/states';
@@ -16,6 +18,7 @@ import {BarButton} from './bar-button';
 import {TimesIcon} from './icons';
 import {PrioritySelection} from './priority-selection';
 import {TextInput} from './text-input';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
 export interface CreateTodoModalProps {
   visible: boolean;
@@ -27,46 +30,76 @@ export const CreateTodoModal = (props: CreateTodoModalProps): ReactElement => {
   const {visible, onCreate, onClose} = props;
   const [task, setTaskText] = useState('');
   const [priority, setPriority] = useState('medium' as TodoPriority);
+  const [dueTo, setDueTo] = useState(new Date());
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const handleConfirm = useCallback(() => setShowDatePicker(false), []);
+  const handleCancel = useCallback(() => setShowDatePicker(false), []);
+
   return (
     <Modal isVisible={visible} style={styles.container}>
       <SafeAreaView style={styles.contentContainer}>
-        <View style={styles.headerContainer}>
-          <TouchableOpacity style={styles.closeIcon} onPress={onClose}>
-            <TimesIcon color={Colors.blue1} size={24} />
-          </TouchableOpacity>
-          <Text style={[CharacterStyles.screenTitle, styles.modalTitle]}>
-            Todo
-          </Text>
-          <View style={{width: 24}} />
-        </View>
-        <View style={styles.sectionContainer}>
-          <Text style={[CharacterStyles.sectionTitle, styles.sectionTitle]}>
-            Things to do
-          </Text>
-          <TextInput
-            placeholder="Your task"
-            onChangeText={setTaskText}
-            autoFocus
-          />
-          <Text
-            style={[
-              CharacterStyles.sectionTitle,
-              styles.sectionTitle,
-              {marginTop: 24},
-            ]}>
-            Priority
-          </Text>
-          <PrioritySelection
-            selectedPriority={priority}
-            onChange={setPriority}
-          />
-        </View>
+        <ScrollView>
+          <View style={styles.headerContainer}>
+            <TouchableOpacity style={styles.closeIcon} onPress={onClose}>
+              <TimesIcon color={Colors.blue1} size={24} />
+            </TouchableOpacity>
+            <Text style={[CharacterStyles.screenTitle, styles.modalTitle]}>
+              Todo
+            </Text>
+            <View style={{width: 24}} />
+          </View>
+          <View style={styles.sectionContainer}>
+            <Text style={[CharacterStyles.sectionTitle, styles.sectionTitle]}>
+              Things to do
+            </Text>
+            <TextInput
+              placeholder="Your task"
+              onChangeText={setTaskText}
+              autoFocus
+              maxLength={120}
+            />
+            <Text
+              style={[
+                CharacterStyles.sectionTitle,
+                styles.sectionTitle,
+                {marginTop: 24},
+              ]}>
+              Priority
+            </Text>
+            <PrioritySelection
+              selectedPriority={priority}
+              onChange={setPriority}
+            />
+            <Text
+              style={[
+                CharacterStyles.sectionTitle,
+                styles.sectionTitle,
+                {marginTop: 24},
+              ]}>
+              Due to
+            </Text>
+            <TouchableWithoutFeedback onPress={() => setShowDatePicker(true)}>
+              <View style={styles.datePickerContainer}>
+                <Text style={[CharacterStyles.text]}>
+                  {dueTo.toTimeString()}
+                </Text>
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
+        </ScrollView>
         <View style={styles.footerContainer}>
           <BarButton
             title="Create todo"
             onPress={(): void => onCreate && onCreate(task)}
           />
         </View>
+        <DateTimePickerModal
+          isVisible={showDatePicker}
+          mode="datetime"
+          minimumDate={new Date()}
+          onConfirm={handleConfirm}
+          onCancel={handleCancel}
+        />
       </SafeAreaView>
     </Modal>
   );
@@ -81,7 +114,7 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     backgroundColor: Colors.white,
-    height: HEIGHT,
+    flex: 1,
     ...Platform.select({
       android: {
         paddingTop: 16,
@@ -111,4 +144,5 @@ const styles = StyleSheet.create({
     color: Colors.gray3,
   },
   closeIcon: {},
+  datePickerContainer: {},
 });
